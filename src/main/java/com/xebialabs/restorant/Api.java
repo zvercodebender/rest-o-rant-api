9ad6c917d11bcf264f5ed6d0027243e79ffc7a30
Vincent Partington <vpartington@xebialabs.com>
2016-02-29 14:03:40 +0100

    Initial commit of REST-o-RANT API.

---
+++ src/main/java/com/xebialabs/restorant/Api.java
@@ -0,0 +1,13 @@
+package com.xebialabs.restorant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Api.class, args);
+    }
+
+}
