e91a6551838f3c6c95974d0c718957ce03fcb1c0
Vincent Partington <vpartington@xebialabs.com>
2016-02-29 14:48:47 +0100

    Building Docker image. - Set tabs to 2.

--- src/main/java/com/xebialabs/restorant/Restaurant.java
+++ src/main/java/com/xebialabs/restorant/Restaurant.java
@@ -2,24 +2,25 @@ package com.xebialabs.restorant;
 
 public class Restaurant {
 
-    private final String name;
-    private final String snippet;
+  private final String name;
+  private final String snippet;
 
-    public Restaurant(String name, String snippet) {
-        this.name = name;
-        this.snippet = snippet;
-    }
+  public Restaurant(String name, String snippet) {
+    this.name = name;
+    this.snippet = snippet;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getSnippet() {
-        return snippet;
-    }
+  public String getSnippet() {
+    return snippet;
+  }
 
-    public boolean matches(String q) {
-        q = q.toLowerCase();
-        return name.toLowerCase().contains(q) || snippet.toLowerCase().contains(q);
-    }
+  public boolean matches(String q) {
+    q = q.toLowerCase();
+    return name.toLowerCase().contains(q) || snippet.toLowerCase().contains(q);
+  }
+
 }
