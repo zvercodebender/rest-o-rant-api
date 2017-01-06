f08a395cb8dc26beaa5f33f6c31c4e93960eccf3
adityakalia <aditya.kalia@gmail.com>
2017-01-06 11:04:13 +0530

    DEPL-10641 Deliver rest-o-rant-api as a WAR file

--- src/main/java/com/xebialabs/restorant/Api.java
+++ src/main/java/com/xebialabs/restorant/Api.java
@@ -2,9 +2,16 @@ package com.xebialabs.restorant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Api {
+public class Api  extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Api.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(Api.class, args);
